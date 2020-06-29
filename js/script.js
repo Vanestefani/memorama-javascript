1a3ad5ee96f2cc36c4d3824d5ac5d9e08840cf39
vanessa <vanessa.estefania.corredor@gmail.com>
2020-06-28 20:23:34 -0500

    propiedades definidas

---
+++ js/script.js
@@ -0,0 +1,16 @@
+class Memorama {
+
+    constructor() {
+        this.totalTargetas = [];
+        this.numeroTargetas = 0;
+        this.verificaTargetas = [];
+        this.errores = 0;
+        this.dificultad = "";
+        this.targetasCorrectas = [];
+        this.agregarTargetas = [];
+    }
+}
+
+new Memorama {
+
+};
