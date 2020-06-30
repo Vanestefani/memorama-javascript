5b02b4feea6e9760416359307dc72409baa8ed7d
vanessa <vanessa.estefania.corredor@gmail.com>
2020-06-29 19:02:14 -0500

    obtener imagen a la cual se dio click

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 class Memorama {
   constructor() {
     /**Propiedades  */
-    this.totalTargetas = [];
-    this.numeroTargetas = 0;
-    this.verificaTargetas = [];
+    this.totalTarjetas = [];
+    this.numeroTarjetas = 0;
+    this.verificaTarjetas = [];
     this.errores = 0;
     this.dificultad = "";
-    this.targetasCorrectas = [];
-    this.agregarTargetas = [];
+    this.tarjetasCorrectas = [];
+    this.agregarTarjetas = [];
     /**Propiedades HTML */
-    this.$contenedorTargetas = document.querySelector(".contenedor-tarjetas");
+    this.$contenedorTarjetas = document.querySelector(".contenedor-tarjetas");
     this.$contenedorGeneral = document.querySelector(".contenedor-general");
     this.$mensaje = document.querySelector("h2 .mensaje");
     this.$pantallaBloqueada = document.querySelector("h2 .pantalla-bloqueada");
@@ -26,23 +26,36 @@ class Memorama {
     /*Extraer datos de json */
     const respuesta = await fetch("../memo.json");
     const data = await respuesta.json();
-    this.totalTargetas = data;
+    this.totalTarjetas = data;
     /*Orden aleatorio */
-    if (this.totalTargetas.length > 0) {
-      this.totalTargetas.sort(orden);
+    if (this.totalTarjetas.length > 0) {
+      this.totalTarjetas.sort(orden);
       function orden(a, b) {
         return Math.random() - 0.5;
       }
     }
-    /*saber el numero de targetas*/
-    this.numeroTargetas = this.totalTargetas.length;
-    /*Ubicacion de targetas (template)*/
-    let html = '';
-    this.totalTargetas.forEach(card => {
-      html += `<div class="tarjeta"><img class="tarjeta-img" src=${card.src} alt="imagenes"></div>`
-
-    })
-    this.$contenedorTargetas.innerHTML=html;
+    /*saber el numero de tarjetas*/
+    this.numeroTarjetas = this.totalTarjetas.length;
+    /*Ubicacion de tarjetas (template)*/
+    let html = "";
+    this.totalTarjetas.forEach((card) => {
+      html += `<div class="tarjeta"><img class="tarjeta-img" src=${card.src} alt="imagenes"></div>`;
+    });
+    this.$contenedorTarjetas.innerHTML = html;
+    this.comienzaJuego();
+  }
+  /*al darle click alas tarjetas */
+  comienzaJuego() {
+    let tarjeta = document.querySelectorAll(".tarjeta");
+    tarjeta.forEach((targeta) => {
+      targeta.addEventListener("click", (e) => {
+        this.clickTarjeta(e);
+      });
+    });
+  }
+  clickTarjeta(e) {
+    let sourceImage = e.target.childNodes[0].attributes[1].value;
+    console.log(sourceImage);
   }
 }
 
